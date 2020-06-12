2f3c8fa6398e076c1538eff62681c971f403ac14
thomaspetty <thomaskpetty@gmail.com>
2020-06-11 20:06:16 -0500

    added validation for model data
    -server side validation is necissary to ensure data integrety
    -each field in the model now has validation and a friednly  message displayed to support valid user inputs
    -prevents clientside bypassing input validation

--- src/main/java/org/launchcode/spaday/models/User.java
+++ src/main/java/org/launchcode/spaday/models/User.java
@@ -1,9 +1,21 @@
 package org.launchcode.spaday.models;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
 public class User {
+    @NotBlank
+    @Size(min=5, max=15)
     private String username;
+    @Email
     private String email;
+    @NotBlank
+    @Size(min = 6)
     private String password;
+    @NotNull(message = "Passwords do not match.")
+    private String verifyPassword;
 
     public User() {
 
@@ -14,6 +26,21 @@ public class User {
         this.username = username;
         this.email = email;
         this.password = password;
+    }
+
+    private void checkPassword() {
+        if(password != null && verifyPassword != null && !password.equals(verifyPassword)){
+            verifyPassword = null;
+        }
+    }
+
+    public String getVerifyPassword() {
+        return verifyPassword;
+    }
+
+    public void setVerifyPassword(String verifyPassword) {
+        this.verifyPassword = verifyPassword;
+        checkPassword();
     }
 
     public String getUsername() {
@@ -38,5 +65,6 @@ public class User {
 
     public void setPassword(String password) {
         this.password = password;
+        checkPassword();
     }
 }
